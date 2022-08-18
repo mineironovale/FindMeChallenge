e8426015855103f53107633b7310801e34837578
mineironovale <augustomessias@pm.me>
2022-08-18 16:16:08 -0300

    Criar service -> UpdateCustomerService para atualizar os dados do cliente

---
+++ src/app/services/customer/UpdateCustomer.js
@@ -0,0 +1,26 @@
+const ListCustomerService = require("../../services/customer/ListCustomerService");
+
+const UpdateCustomerService = {
+    update: (
+        id,
+        name
+      ) => {
+        const allCustomers = ListCustomerService.listAll();
+        const customerIndex = allCustomers.findIndex(item => item.id === Number(id))
+    
+        if (customerIndex === -1) {
+          return { erro: "Cliente não encontrado" }
+        }
+    
+        allCustomers[customerIndex] = {
+          name
+        }
+    
+        return {
+          id,
+          name
+        }
+      }
+};
+
+module.exports = UpdateCustomerService;
