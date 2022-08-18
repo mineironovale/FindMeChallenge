f7261eed7a0826bfbd4809fba29f1d4cad57dc82
mineironovale <augustomessias@pm.me>
2022-08-18 16:21:53 -0300

    Criar service -> DeleteCustomerService para excluir um determinado usuário

---
+++ src/app/services/customer/DeleteCustomerService.js
@@ -0,0 +1,17 @@
+const ListCustomerService = require("../../services/customer/ListCustomerService");
+
+const DeleteCustomerService = {
+    delete: (id) => {
+        const allCustomers = ListCustomerService.listAll();
+        const customerIndex = allCustomers.findIndex(item => item.id === Number(id));
+
+        if(customerIndex === -1) {
+            return { erro: "Cliente não encontrado" }
+        }
+
+        allCustomers.splice(customerIndex, 1)
+        return { mensagem: "Cliente removido com sucesso" }
+    }
+}
+
+module.exports = DeleteCustomerService;
