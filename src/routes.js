7a963ce88ad79940280961244d6048f43b8ccd39
mineironovale <augustomessias@pm.me>
2022-08-19 09:49:35 -0300

    Adicionar arquivo de rotas em routes.js

---
+++ src/routes.js
@@ -0,0 +1,25 @@
+const Router = require("express");
+const routes = new Router();
+
+const customerController = require("./app/controllers/customer/CustomerController");
+const employeeController = require("./app/controllers/employee/EmployeeController");
+
+const customerValidator = require("./middlewares/customer/CustomerValidator");
+const employeeValidator = require("./middlewares/employee/EmployeeValidator");
+
+routes.get("/customer", customerController.index);
+routes.get("/customer", customerController.ListId);
+routes.get("/customer", customerController.ListName);
+routes.post("/customer", customerValidator, customerController.create);
+routes.put("/customer", customerController.update);
+routes.delete("/customer", customerController.delete);
+
+routes.get("/employee", employeeController.index);
+routes.get("/employee", employeeController.ListId);
+routes.get("/employee", employeeController.ListName);
+routes.get("/employee", employeeController.ListEmail);
+routes.post("/employee", employeeValidator, employeeController.create);
+routes.put("/employee", employeeController.update);
+routes.delete("/employee", employeeController.delete);
+
+module.exports = routes;
