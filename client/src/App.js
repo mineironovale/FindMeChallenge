a6179b186e3e7cd88cf075b3ffc1a85892ba5066
mineironovale <augustomessias@pm.me>
2022-08-20 17:38:09 -0300

    Criar variáveis para acessar os valores digitados na tela de cadastro

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,18 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [customerName, setCustomerName] = useState("");
+
+  const [employeeName, setEmployeeName] = useState("");
+  const [employeeEmail, setEmployeeEmail] = useState("");
+  const [employeePassword, setEmployeePassword] = useState("");
+
+  const [orderDate, setOrderDate] = useState("");
+  const [orderCustomerId, setOrderCustomerId] = useState("");
+  const [orderDescription, setOrderDescription] = useState("");
+  const [orderEmployeeId, setOrderEmployeeId] = useState("");
+
   return (
     <div className="App">
       <div>
@@ -10,32 +22,72 @@ function App() {
         <h2>Cadastro</h2>
         <h3>Cliente</h3>
         <label>Nome:</label>
-        <input type="text" />
+        <input
+          type="text"
+          onChange={(event) => {
+            setCustomerName(event.target.value);
+          }}
+        />
         <button>Adicionar cliente</button>
 
         <h3>Colaborador</h3>
         <label>Nome:</label>
-        <input type="text" />
+        <input
+          type="text"
+          onChange={(event) => {
+            setEmployeeName(event.target.value);
+          }}
+        />
         <label>E-mail:</label>
-        <input type="text" />
+        <input
+          type="text"
+          onChange={(event) => {
+            setEmployeeEmail(event.target.value);
+          }}
+        />
         <label>Senha:</label>
-        <input type="text" />
+        <input
+          type="text"
+          onChange={(event) => {
+            setEmployeePassword(event.target.value);
+          }}
+        />
         <button>Adicionar colaborador</button>
 
         <h3>Ordem de Serviço</h3>
         <label>Data de abertura:</label>
-        <input type="date" />
+        <input
+          type="text"
+          onChange={(event) => {
+            setOrderDate(event.target.value);
+          }}
+        />
         <label>Id do cliente:</label>
-        <input type="number" />
+        <input
+          type="text"
+          onChange={(event) => {
+            setOrderCustomerId(event.target.value);
+          }}
+        />
         <label>Problema relatado:</label>
-        <input type="text" />
+        <input
+          type="text"
+          onChange={(event) => {
+            setOrderDescription(event.target.value);
+          }}
+        />
         <label>Id do colaborador designado para o problema:</label>
-        <input type="number" />
+        <input
+          type="text"
+          onChange={(event) => {
+            setOrderEmployeeId(event.target.value);
+          }}
+        />
         <button>Adicionar Ordem de Serviço</button>
       </div>
-      <div className="search">
+      {/* <div className="search">
         <h2>Pesquisa</h2>
-      </div>
+      </div> */}
     </div>
   );
 }
