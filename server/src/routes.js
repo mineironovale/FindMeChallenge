33dc67429024a2a6fcdf3a5635a767df532430ce
mineironovale <augustomessias@pm.me>
2022-08-21 22:14:14 -0300

    Adicionar rotas para ordens de serviço em routes.js

--- server/src/routes.js
+++ server/src/routes.js
@@ -5,8 +5,8 @@ import customerController from "./app/controllers/customer/CustomerController";
 import employeeController from "./app/controllers/employee/EmployeeController";
 import orderController from "./app/controllers/order/OrderController"; 
 
-routes.get("/customers", customerController.index);
-routes.get("/customer/:id", customerController.ListId);
+routes.get("/customers", customerController.readAll);
+routes.get("/customer/:id", customerController.readId);
 routes.post("/customer/create", customerController.create);
 routes.put("/customer/update", customerController.update);
 routes.delete("/customer/delete", customerController.delete);
@@ -16,5 +16,11 @@ routes.get("/employee/:id", employeeController.ListId);
 routes.post("/employee/create", employeeController.create);
 routes.put("/employee/update", employeeController.update);
 routes.delete("/employee/delete", employeeController.delete);
+
+routes.get("/orders", orderController.index);
+routes.get("/order/:id", orderController.ListId);
+routes.post("/order/create", orderController.create);
+routes.put("/order/update", orderController.update);
+routes.delete("/order/delete", orderController.delete);
 
 export default routes;
